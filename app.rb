363daf34b5fe4686b00d086e7a01dd2b949393f7
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-02-01 13:28:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,6 +12,6 @@ class App < Sinatra::Base
   end
 
   get '/friends' do
-    @friends
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
   end
 end
